f4024fee3ec77e1dbd1cd14f8ce837a309de58ff
Cleverton Bezerra <cleverton.inacio.110@ufrn.edu.br>
2021-08-25 12:18:55 -0300

    #10 Adiciona rota Carrinho

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -17,6 +17,9 @@ const routes: Routes = [
             {
                 path: 'products', loadChildren: () => import('../products/products.module').then(m => m.ProductsModule)
             },
+            {
+                path: 'cart', loadChildren: () => import('../cart/cart.module').then(m => m.CartModule)
+            },
         ]
     }
 ];
