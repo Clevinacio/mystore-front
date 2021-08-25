ccc6eb66913b7083987b85f10eb078acb1068913
Cleverton Bezerra <cleverton.inacio.110@ufrn.edu.br>
2021-08-25 12:09:46 -0300

    #10 Adiciona rota products

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -14,6 +14,9 @@ const routes: Routes = [
             {
                 path: 'home', loadChildren: () => import('../home/home.module').then(m => m.HomeModule)
             },
+            {
+                path: 'products', loadChildren: () => import('../products/products.module').then(m => m.ProductsModule)
+            },
         ]
     }
 ];
