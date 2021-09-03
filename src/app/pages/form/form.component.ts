c0289e89065878e51e2f72b01ccf6a3be64a0f32
Cleverton Bezerra <cleverton.inacio.110@ufrn.edu.br>
2021-09-03 16:30:41 -0300

    #5 - Cria formulário para criação de conta com validação

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+    selector: 'app-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private router: Router) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+
+    }
+
+    validate(e: Event) {
+        var form = document.getElementsByClassName('needs-validation')[0] as HTMLFormElement;
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        } else {
+            this.router.navigate(['/home'])
+        }
+        form.classList.add('was-validated');
+    }
 
 }
