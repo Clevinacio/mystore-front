7a2f405327d163550c690a73c42eb0bc7f144add
Cácio Lucas <cclucas060901@gmail.com>
2021-09-08 16:08:03 -0300

    Rebase de branch main na c607ba5

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Customer } from 'src/app/models/customer';
+import { CustomerService } from 'src/app/services/customer.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-form',
@@ -8,19 +11,43 @@ import { Router } from '@angular/router';
 })
 export class FormComponent implements OnInit {
 
-    constructor(private router: Router) { }
-
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private customerService: CustomerService,
+        private modalService: NgbModal) { }
     ngOnInit(): void {
 
+    }
+    //@ts-ignore
+    @ViewChild('content') public content: TemplateRef<any>;
+
+    openSuccessModal() {
+        this.modalService.open(this.content, { centered: true });
+    }
+
+    homeRedirect() {
+        this.modalService.dismissAll();
+        this.router.navigate(['/home'])
     }
 
     validate(e: Event) {
         var form = document.getElementsByClassName('needs-validation')[0] as HTMLFormElement;
+        e.preventDefault();
         if (form.checkValidity() === false) {
             e.preventDefault();
             e.stopPropagation();
         } else {
-            this.router.navigate(['/home'])
+            const customer = {
+                first_name: (<HTMLInputElement>document.getElementById('first_name')).value,
+                last_name: (<HTMLInputElement>document.getElementById('last_name')).value,
+                username: (<HTMLInputElement>document.getElementById('username')).value,
+                password: (<HTMLInputElement>document.getElementById('password')).value,
+                email: (<HTMLInputElement>document.getElementById('email')).value,
+            }
+            this.customerService.createCustomer(<Customer>customer).subscribe((customer: Customer) => {
+                this.openSuccessModal();
+            });
         }
         form.classList.add('was-validated');
     }
