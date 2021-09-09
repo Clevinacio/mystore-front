97e62c6ff81c6a19c2f3c49687a2f55f1691db3c
Cácio Lucas <cclucas060901@gmail.com>
2021-09-09 12:54:21 -0300

    Botão para deslogar e remover botão lixo

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { faShoppingCart, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { faPowerOff, faShoppingCart, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-header',
@@ -9,11 +10,17 @@ import { faShoppingCart, IconDefinition } from '@fortawesome/free-solid-svg-icon
 export class HeaderComponent implements OnInit {
     isMenuCollapsed = true;
     faCart: IconDefinition = faShoppingCart;
+    faOff: IconDefinition = faPowerOff;
     nome: string = 'User full_name';
-    constructor() { }
+    constructor(private router: Router,) { }
 
     ngOnInit(): void {
       this.nome = JSON.parse(<string>sessionStorage.getItem('customer'))['first_name'] + ' ' + JSON.parse(<string>sessionStorage.getItem('customer'))['last_name']
+    }
+
+    logout() {
+      sessionStorage.removeItem('customer');
+      this.router.navigate(['/home']);
     }
 
 }
