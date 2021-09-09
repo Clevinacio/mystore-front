77ebcf1a3820c5a6bfde14c6dd86029eaa0b2763
Cleverton Inácio Bezerra <cleverton.inacio.110@ufrn.edu.br>
2021-09-09 14:13:19 -0300

    #28 - Adiciona layout

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,15 +1,27 @@
+import { Router } from '@angular/router';
+import { CartService } from './../../services/cart.service';
+import { CartItem } from './../../models/cart-item';
+import { Product } from './../../models/product';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+    selector: 'app-product-details',
+    templateUrl: './product-details.component.html',
+    styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+    constructor(private modalService: NgbModal, private cartService: CartService, private router: Router) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    addToCart(content: any) {
+        this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+    }
+
+    redirectToCart() {
+        this.router.navigate(['/cart']);
+    }
 }
