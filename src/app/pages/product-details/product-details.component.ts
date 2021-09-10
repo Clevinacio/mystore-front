4283b5dabe7b5e2707bd5b91ffcf4889ad29c66f
Cleverton Inácio Bezerra <cleverton.inacio.110@ufrn.edu.br>
2021-09-09 21:26:52 -0300

    #28 - Implementa botão comprar

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,3 +1,4 @@
+import { CartItem } from './../../models/cart-item';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CartService } from './../../services/cart.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -11,24 +12,34 @@ import { Product } from 'src/app/models/product';
     styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+    cartSize: number = 0;
 
     constructor(
-      private modalService: NgbModal, 
-      private cartService: CartService, 
-      private router: Router,
-      private route: ActivatedRoute,
-      private productService: ProductService
-      ) { }
-    
-      product: Product|null = null;
-  
-      ngOnInit(): void {
-      this.productService.getProductById(this.route.snapshot.params.id).subscribe(product => {
-        this.product = product;
-      });
+        private modalService: NgbModal,
+        private cartService: CartService,
+        private router: Router,
+        private route: ActivatedRoute,
+        private productService: ProductService
+    ) { }
+
+    product: Product = {} as Product;
+
+    ngOnInit(): void {
+        this.productService.getProductById(this.route.snapshot.params.id).subscribe(product => {
+            this.product = product;
+            this.cartSize = this.cartService.getCartItems.length;
+        });
     }
 
-    addToCart(content: any) {
+    addToCart(content: any, product: Product) {
+        let cartItem: CartItem = {
+            id: this.cartSize + 1,
+            quantity: 1,
+            user: JSON.parse(<string>sessionStorage.getItem('customer'))['id'],
+            product: product.id
+        }
+
+        this.cartService.addCartItem(cartItem).subscribe();
         this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
     }
 
